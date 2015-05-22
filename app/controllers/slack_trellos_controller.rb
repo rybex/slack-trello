a7fdb8e195910363e77e14c42ace26ae2c62f06f
rybex <tomek.rybka@gmail.com>
2015-05-22 15:50:58 +0200

    Created controller action to catch webhooks

--- app/controllers/slack_trellos_controller.rb
+++ app/controllers/slack_trellos_controller.rb
@@ -1,6 +1,41 @@
 class SlackTrellosController < ApplicationController
 
   def create_card
-    logger.debug "Hello world!" + params[:user_name]
+    form = validate_request
+    create_trello_card(form)
+  rescue RequestForm::RequestInvalid
+    respond_to do |format|
+      format.json do
+        render json: {text: 'Wrong request params. Something is missing'}
+      end
+    end
+  rescue CreateTrelloCard::BoardNotFound
+    respond_to do |format|
+      format.json do
+        render json: {text: 'Board not exist'}
+      end
+    end
+  rescue CreateTrelloCard::BoardListNotFound
+    respond_to do |format|
+      format.json do
+        render json: {text: 'Boards list not exist'}
+      end
+    end
+  end
+
+  private
+
+  def validate_request
+    form = RequestForm.new(params)
+    form.validate!
+    form
+  end
+
+  def create_trello_card(form)
+    CreateTrelloCard.new(form, trello_client).call
+  end
+
+  def trello_client
+    TrelloClient.new(ENV['MEMBER'])
   end
 end
